99feed7e9a16138c4fa79e4184e7ceadfc480026
Marlisson Silva <marlyssonsilva005@gmail.com>
2022-09-30 08:05:44 -0300

    Opções de seções de about e de produtos

--- js/script.js
+++ js/script.js
@@ -7,3 +7,40 @@ function showMenu(event) {
 }
 btnMobile.addEventListener('touchstart', showMenu)
 btnMobile.addEventListener('click', showMenu)
+
+// HIDE MENU ON CLICK
+const menuItems = document.querySelectorAll('.menu a');
+
+menuItems.forEach(item => {
+      item.addEventListener('click', hideMenu);
+})
+
+function hideMenu(event) {
+      if (event.type === 'touchstart') event.preventDefault()
+      const nav = document.querySelector('.nav')
+      nav.classList.remove('active')
+}
+
+
+// SCROLL MENU
+menuItems.forEach(item => {
+      item.addEventListener('click', scrollClick);
+})
+
+function scrollClick(event) {
+      event.preventDefault();
+      const element = event.target;
+      const id = element.getAttribute('href')
+      const to = document.querySelector(id).offsetTop;
+
+      window.scroll({
+            top: to - 65,
+            behavior: "smooth",
+      })
+}
+
+// 
+
+
+
+
